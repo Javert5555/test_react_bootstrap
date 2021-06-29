890850b1f106906134bd7a43747c57b4eabb664b
Maxwell Rutherford <javert.hugo@yandex.ru>
2021-06-29 15:43:41 +0300

    add navbar

---
+++ src/Components/Navibar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Nav, Navbar, Button } from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+
+export default function NaviBar () {
+
+    return (
+        <>
+            <Navbar collapseOnSelect expend='lg' bg='dark' variant='dark'>
+                <Navbar.Brand>Test</Navbar.Brand>
+                <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
+                <Navbar.Collapse id='responsive-navbar-nav'>
+                    <Nav className='mr-auto'>
+                        <Nav.Link><Link to='/'>Home</Link></Nav.Link>
+                        <Nav.Link><Link to='/users'>Users</Link></Nav.Link>
+                        <Nav.Link><Link to='/about'>About</Link></Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Button variant='primary' className='mr-2'>Log In</Button>
+                        <Button variant='primary'>Sign Out</Button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    );
+}
